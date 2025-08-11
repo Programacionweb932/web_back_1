527446c50f06ddd0d3bfcef750b0dfa9eb5e3726
Blanca Florez <alexanderloaiza@MacBook-Air-de-Alexander.local>
2025-08-11 16:45:44 -0500

    Se modifica el server.js y el vercel.json

--- server.js
+++ server.js
@@ -54,6 +54,10 @@ mongoose.connect(mongoURI, {
 })
   .then(() => console.log('✅ Conectado a MongoDB'))
   .catch(err => console.error('❌ Error al conectar a MongoDB:', err));
+  app.get('/', (req, res) => {
+  res.send('Servidor backend funcionando 🚀');
+});
+
 
 // Exportar para Vercel
 module.exports = app;
